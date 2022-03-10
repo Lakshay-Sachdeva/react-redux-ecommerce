735856054ee8736cebe77c1c76a7c945636ca368
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2022-03-11 00:32:39 +0530

    style added to details page

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -6,14 +7,31 @@ export const ProductComponent = () => {
 
   const renderList = products.map((el) => {
     return (
-      <Link key={el.id} to={`/products/${el.id}`}>
-        <figure>
-          <img src={el.image} alt="" />
-          <figcaption>{el.description}</figcaption>
-        </figure>
-      </Link>
+      <Container>
+        <Link key={el.id} to={`/products/${el.id}`}>
+          <Figure>
+            <Image src={el.image} alt="" />
+            <figcaption>{el.description}</figcaption>
+          </Figure>
+        </Link>
+      </Container>
     );
   });
 
   return <>{renderList}</>;
 };
+
+const Container = styled.div`
+  display: flex;
+  gap: 5%;
+  postion: absolute;
+`;
+
+const Figure = styled.figure`
+  position: relative;
+  width: 20%;
+`;
+
+const Image = styled.img`
+  width: 80%;
+`;
--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -18,16 +19,49 @@ export const ProductDetails = () => {
   }, [productId]);
 
   return (
-    <div>
-      <div>
-        <img src={product.image} alt={product.title} />
-      </div>
-      <div>
+    <Container>
+      <ImageDiv>
+        <Image src={product.image} alt={product.title} />
+      </ImageDiv>
+      <DataDiv>
         <h1>{product.title}</h1>
         <h4>{product.description}</h4>
-        {/* <h4>Rating: {product.rating.rate}</h4> */}
         <h4>Price: $ {product.price}</h4>
-      </div>
-    </div>
+      </DataDiv>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  gap: 10%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageDiv = styled.div`
+  width: 30vw;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+
+const DataDiv = styled.div`
+  width: 30vw;
+  marign- left: 50px;
+
+  h1{
+    font-size: 26px;
+    font-weight: 600;
+    color: rgb(7, 34, 39)
+  }
+
+  h4{
+    font-size: 20px;
+    font-weight: 300;
+    marign: 20px auto;
+  }
+
+`;
